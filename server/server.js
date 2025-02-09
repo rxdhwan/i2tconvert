fcfe9964e03b458fc3915c152e6dbe9584ddd6c8
Ridhwan <ridhwanyt@gmail.com>
2025-02-09 04:57:58 +0400

    Updated retrieve and upload filename

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-const uploadImageRouter = require('./routes/uploadImage');
-const retrieveImageRouter = require('./routes/retrieveImage');
+const uploadImageRouter = require('./routes/uploadimage');
+const retrieveImageRouter = require('./routes/retrieveimage');
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
